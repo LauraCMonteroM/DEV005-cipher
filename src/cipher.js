66092b167479a1c88920134289d5c76d6e1fb5dc
unknown <lauramontero205@gmail.com>
2023-02-17 16:56:04 -0500

    Empezamos con el js

--- src/cipher.js
+++ src/cipher.js
@@ -1,5 +1,16 @@
 const cipher = {
-  // ...
+  // Cifrado método encode
+      encode: function(offset, string)
+      {
+          return
+      }
+  //Descifrado método decode
+      decode: function(offset, string)
+      {
+          return
+      }
 };
+
+//buscar fucniones puras
 
 export default cipher;
--- src/index.js
+++ src/index.js
@@ -1,3 +1,37 @@
 import cipher from './cipher.js';
 
+//Elementos del DOM en variables
+    const mensajeaCifrar = document.getElementById('txtacifrar')
+    const mensajeaDescifrar = document.getElementById('txtadcifrar')
+    const btnCifrar = document.getElementById('cifrar')
+    const btnDescifrar = document.getElementById('dcifrar')
+    const desplCifrar = document.getElementById('cifraroffset')
+    const desplDcifrar = document.getElementById('dcifraroffset')
+
+//Funciones que interactúan con el DOM
+    function mostrarCifrado()
+    {
+        const mensajeaCifrar = document.getElementById('txtacifrar').value
+        const desplCifrar = document.getElementById('cifraroffset').value
+        //función que me muestre el cifrado en el div
+    }
+
+    function mostrarDescifrado()
+    {
+        const mensajeaDescifrar = document.getElementById('txtadcifrar').value
+        const desplDcifrar = document.getElementById('dcifraroffset').value
+        //función que me muestre el descifrado en el div
+    }
+
+//Eventos del DOM
+    btnCifrar.addEventListener('click', mostrarCifrado)
+
+    btnDescifrar.addEventListener('click' , mostrarDescifrado)
+
+
 console.log(cipher);
+
+
+
+
+
