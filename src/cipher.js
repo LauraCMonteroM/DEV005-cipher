4807bf79dae6fd57e43f380096a39dcbb76b6f7c
unknown <lauramontero205@gmail.com>
2023-02-23 18:21:10 -0500

    Funcionando sin tests

--- src/cipher.js
+++ src/cipher.js
@@ -1,23 +1,25 @@
 const cipher = {
   // Cifrado
-  encode: function (texto, offset) {
+  encode: function (offset, texto) {
+    const newOffset = parseInt(offset)
     let resultado1 = "";
     for (let i = 0; i < texto.length; i++) {
       const textoMayus = texto.toUpperCase();
       let codigoAscii = textoMayus.charCodeAt(i);
       if (codigoAscii >= 65 && codigoAscii <= 90) {
-        codigoAscii = ((codigoAscii + parseInt(offset) - 65) % 26) + 65;
+        codigoAscii = ((codigoAscii + newOffset - 65) % 26) + 65;
       }
       resultado1 += String.fromCharCode(codigoAscii);
     }
     return resultado1;
-  },  decode: function (texto, offset) {
+  },
+  decode: function (offset, texto) {
+    const realOffset = parseInt(offset);
     let resultado2 = "";
-    for (letra = 0; letra < texto.length; letra++) {
-      const textoCif = texto.toUpperCase();
-      let codigoAscii1 = textoCif.charCodeAt(letra);
+    for (let letra = 0; letra < texto.length; letra++) {
+      let codigoAscii1 = texto.charCodeAt(letra);
       if (codigoAscii1 >= 65 && codigoAscii1 <= 90) {
-        codigoAscii1 = ((codigoAscii1 - parseInt(offset) - 65) % 26) + 65;
+        codigoAscii1 = ((codigoAscii1 - 65 - realOffset) % 26 + 26) % 26 + 65;
       }
       resultado2 += String.fromCharCode(codigoAscii1);
     }
--- src/index.js
+++ src/index.js
@@ -1,37 +1,28 @@
-import cipher from './cipher.js';
+import cipher from "./cipher.js";
 
 //Elementos del DOM en variables
-    const btnCifrar = document.getElementById('cifrar')
-    const btnDescifrar = document.getElementById('dcifrar')
-    const resultadoCifrar = document.getElementById ('muestra-cifrado')
-    const resultadoDescifrar = document.getElementById ('muestra-descifrado')
+const btnCifrar = document.getElementById("cifrar");
+const btnDescifrar = document.getElementById("dcifrar");
+const resultadoCifrar = document.getElementById("muestra-cifrado");
+const resultadoDescifrar = document.getElementById("muestra-descifrado");
 
 //Funciones que interactúan con el DOM
-    function mostrarCifrado()
-    {
-        const mensajeaCifrar = document.getElementById('txtacifrar').value
-        const desplCifrar = document.getElementById('cifraroffset').value
-        //función que me muestre el cifrado en el div .cipher.encode(offset, string)
-        resultadoCifrar.innerHTML = cipher.encode(mensajeaCifrar, desplCifrar);
-    }
+function mostrarCifrado() {
+  const mensajeaCifrar = document.getElementById("txtacifrar").value;
+  const desplCifrar = document.getElementById("cifraroffset").value;
+  //función que me muestre el cifrado en el div .cipher.encode(offset, string)
+  resultadoCifrar.innerHTML = cipher.encode(desplCifrar, mensajeaCifrar);
+}
 
-    function mostrarDescifrado()
-    {
-        const mensajeaDescifrar = document.getElementById('txtadcifrar').value
-        const desplDcifrar = document.getElementById('dcifraroffset').value
-        //función que me muestre el cifrado en el div .cipher.encode(offset, string)
-        resultadoDescifrar.innerHTML = cipher.decode(mensajeaDescifrar, desplDcifrar)
-    }
+function mostrarDescifrado() {
+  const mensajeaDescifrar = document.getElementById("txtadcifrar").value;
+  const desplDcifrar = document.getElementById("dcifraroffset").value;
+  //función que me muestre el cifrado en el div .cipher.encode(offset, string)
+  resultadoDescifrar.innerHTML = cipher.decode(desplDcifrar, mensajeaDescifrar);
+}
 
 //Eventos del DOM
-    btnCifrar.addEventListener('click', mostrarCifrado)
+btnCifrar.addEventListener("click", mostrarCifrado);
 
-    btnDescifrar.addEventListener('click' , mostrarDescifrado)
-
-
-console.log(cipher);
-
-
-
-
+btnDescifrar.addEventListener("click", mostrarDescifrado);
 
